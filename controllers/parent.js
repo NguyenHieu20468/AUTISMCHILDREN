724f28e0729d4aca82d356fd1e5d064c6d018bd3
187PM13983 - Phan Nguyen Thi Yen Nhi - K24T-PM1 <nhi.187pm13983@vanlanguni.vn>
2020-07-19 04:39:28 +0700

    complete convert view html to ejs

--- controllers/parent.js
+++ controllers/parent.js
@@ -10,4 +10,36 @@ router.get('/', async (req, resp) => {
     resp.render('../views/trangchu.ejs');
 });
 
+router.get('/dangnhap', async (req, resp) => {
+    resp.render('../views/dangnhap.ejs');
+});
+
+router.get('/dangky', async (req, resp) => {
+    resp.render('../views/dangky.ejs');
+});
+
+router.get('/baigiang', async (req, resp) => {
+    resp.render('../views/baigiang.ejs');
+});
+
+router.get('/gioithieu', async (req, resp) => {
+    resp.render('../views/gioithieu.ejs');
+});
+
+router.get('/lamkiemtra', async (req, resp) => {
+    resp.render('../views/lamkiemtra.ejs');
+});
+
+router.get('/lienhe', async (req, resp) => {
+    resp.render('../views/lienhe.ejs');
+});
+
+router.get('/themhosotre', async (req, resp) => {
+    resp.render('../views/themhosotre.ejs');
+});
+
+router.get('/thuvien', async (req, resp) => {
+    resp.render('../views/thuvien.ejs');
+});
+
 module.exports = router;
