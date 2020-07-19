076aa52fc9fd5c88ab2a254c55524a47535bb3b9
187PM13983 - Phan Nguyen Thi Yen Nhi - K24T-PM1 <nhi.187pm13983@vanlanguni.vn>
2020-07-19 09:00:42 +0700

    log in (not include redirect)

--- controllers/parent.js
+++ controllers/parent.js
@@ -5,13 +5,81 @@ var objectId = require('mongodb').ObjectID;
 // middleware
 var multer = require('multer');
 var upload = multer({});
+// utils
+var MyUtil = require("../utils/MyUtil.js");
+const session = require('express-session');
+const { validationResult } = require('express-validator');
+// daos
+var pathDAO = "../daos/mongoose";
+var phuhuynhDAO = require(pathDAO + "/phuhuynhDAO.js");
+var treemDAO = require(pathDAO + "/treemDAO.js");
 
 router.get('/', async (req, resp) => {
     resp.render('../views/trangchu.ejs');
 });
 
 router.get('/dangnhap', async (req, resp) => {
-    resp.render('../views/dangnhap.ejs');
+    if (!req.session.phuhuynh) {
+        resp.render('../views/dangnhap.ejs');
+    } else {
+        resp.redirect('/');
+    }
+});
+
+
+router.post('/dangnhap', async (req, resp) => {
+    try{
+        var email = req.body.email;
+        var password = req.body.password;
+        // var pwdhashed = MyUtil.md5(password);
+        // var phuhuynh = await phuhuynhDAO.selectByEmailAndPassword(email, pwdhashed);
+        // if (phuhuynh) {
+        //     console.log('abc');
+        //     req.session.phuhuynh = phuhuynh;
+        //     resp.redirect('/');
+        // } else {
+        //     MyUtil.showAlertAndRedirect(resp, 'Invalid login!', './dangnhap');
+        // }
+    
+        var crypto = require('crypto');
+        var pwdHashed = crypto.createHash('md5').update(password).digest('hex');
+        //connect mongodb
+        var MongoClient = require('mongodb').MongoClient;
+        var uri = "mongodb+srv://admin:Ws63LIR0qnhtCPb1@cluster0.j4ajr.mongodb.net/AutismChildren";
+    
+        let conn = await MongoClient.connect(uri);
+    
+        //if(err) throw err;
+        var db = conn.db('AutismChildren');
+        var query = {email: email, matkhau: pwdHashed};
+        let result = await db.collection('phuhuynh').findOne(query)
+        
+        console.log(result)
+        
+    
+        if(result) {
+            req.session.phuhuynh = result;
+            console.log('logged in')
+            console.log("Complete render")
+            resp.redirect('/');
+        } else {
+            console.log('invalid');
+            resp.render('dangnhap');
+        }
+        conn.close();
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+    
+});
+
+
+
+router.get('/dangxuat', (req, resp) => {
+    delete req.session.phuhuynh;
+    resp.redirect('/');
 });
 
 router.get('/dangky', async (req, resp) => {
